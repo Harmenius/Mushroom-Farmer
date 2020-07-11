a2bcddbcbb59f7530f8cc70f41ce6d56a0399186
Harmen Prins <prins_harmen@hotmail.com>
2020-07-11 23:27:52 +0200

    Make grid and mushrooms variable

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -7,15 +7,17 @@ export (int) var goal_money
 export (int) var time
 export (Vector2) var grid_size
 export (int) var mushroom_time
-export (Vector2) var start_mushroom
+export (Array) var start_mushrooms
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("UI").init_variables(start_money, goal_money, time)
 	MushroomGrid.initialise(grid_size, mushroom_time)
-	MushroomGrid.make_mushroom(start_mushroom.x, start_mushroom.y)
+	for coords in start_mushrooms:
+		MushroomGrid.make_mushroom(coords.x, coords.y)
 	get_node("TileMap").WIDTH = grid_size.x
 	get_node("TileMap").HEIGHT = grid_size.y
+	get_node("TileMap").initialise()
 	randomize()  # Changes result of e.g. .shuffle()
 
 func trigger_loss(reason: String):
--- Scripts/Tiles.gd
+++ Scripts/Tiles.gd
@@ -15,8 +15,7 @@ enum Tiles {GRASS_TILE,
 			CORNER_EN,
 			CORNER_WN}
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func initialise():
 	position = get_viewport().get_size()/2 \
 	 		 - Vector2(WIDTH, HEIGHT)/2*20 \
 			 - Vector2(10,10)  # Half a tile
@@ -25,16 +24,12 @@ func _ready():
 			set_cell(x, y, Tiles.GRASS_TILE)
 	
 	for x in range(WIDTH):
-		set_cell(x, WIDTH, Tiles.HORIZONTAL_FENCE)
+		set_cell(x, HEIGHT, Tiles.HORIZONTAL_FENCE)
 		set_cell(x, -1, Tiles.HORIZONTAL_FENCE)
-		set_cell(-1, x, Tiles.VERTICAL_FENCE)
-		set_cell(HEIGHT, x, Tiles.VERTICAL_FENCE)
+	for y in range(HEIGHT):
+		set_cell(-1, y, Tiles.VERTICAL_FENCE)
+		set_cell(WIDTH, y, Tiles.VERTICAL_FENCE)
 	set_cell(WIDTH, -1, Tiles.CORNER_WS)
 	set_cell(-1, -1, Tiles.CORNER_ES)
 	set_cell(-1, HEIGHT, Tiles.CORNER_EN)
 	set_cell(WIDTH, HEIGHT, Tiles.CORNER_WN)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
